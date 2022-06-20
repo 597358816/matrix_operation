861fe90f02069126f28244c621b7a7e8c4002ff7
597358816 <62841617+597358816@users.noreply.github.com>
2022-06-21 00:37:09 +0800

    Update goe.h
    goe test finished

--- goe.h
+++ goe.h
@@ -1,14 +1,13 @@
 #pragma once
 #include<stdio.h>
 #include<stdlib.h> 
-#include<stack>
 #define LENGTH 8
-struct GOETreeBinary{
+struct GOETreeBinary {
 	int configuration[LENGTH];
 	GOETreeBinary* son[2];
 	GOETreeBinary* father;
 };
-struct StateNode{
+struct StateNode {
 	int configuration[LENGTH];
 	StateNode* next;
 };
@@ -23,46 +22,12 @@ struct GOEQueue {
 	GOEQueueNode* head;
 	GOEQueueNode* tail;
 };
-
+void getRules(int* a);
 GOETreeBinary* initGOENode();
 int recordConfiguration(int* configuration, StateNode* head);
 GOETreeBinary* createGOERoot(int* rules, StateNode* head);
 GOETreeBinary* createGOETree(int* rules, GOETreeBinary* node, StateNode* head, GOEQueue* queue);
 
-GOEQueue* initGOEQueue(GOETreeBinary* elem)
-{
-	GOEQueue* queue = (GOEQueue*)malloc(sizeof(GOEQueue));
-	GOEQueueNode* node = (GOEQueueNode*)malloc(sizeof(GOEQueueNode));
-	node->next = NULL;
-	node->element = elem;
-	//队头有元素
-	queue->head = node;
-	//队尾为空
-	queue->tail = node->next;
-	return queue;
-}
-
-void pushGOEQueue(GOEQueue* queue, GOETreeBinary* elem)
-{
-	GOEQueueNode* node = (GOEQueueNode*)malloc(sizeof(GOEQueueNode));
-	node->element = elem;
-	queue->tail = node;
-	queue->tail = node->next;
-	
-}
-
-GOETreeBinary* popGOEQueue(GOEQueue* queue)
-{
-	//头节点不为空则队列不为空
-	if (queue->head)
-	{
-		GOEQueueNode* temp = queue->head;
-		queue->head = queue->head->next;
-		return temp->element;
-	}
-	else
-	{
-		return nullptr;
-	}
-
-}
+GOEQueue* initGOEQueue();
+void pushGOEQueue(GOEQueue* queue, GOETreeBinary* elem);
+GOETreeBinary* popGOEQueue(GOEQueue* queue);
